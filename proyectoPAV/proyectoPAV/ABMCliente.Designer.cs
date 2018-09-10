9eb6b979f26107eb811e31f089fe09fbacbb2c97
josuebaccapav <josuembacca@outlook.com>
2018-09-10 19:52:13 -0300

    EMPIEZO ABM CLIENTES

---
+++ proyectoPAV/proyectoPAV/ABMCliente.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace proyectoPAV
+{
+    partial class ABMCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtCUIT = new System.Windows.Forms.TextBox();
+            this.txtCodPostal = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.cmdNuevo = new System.Windows.Forms.Button();
+            this.cmdModificar = new System.Windows.Forms.Button();
+            this.cmdEliminar = new System.Windows.Forms.Button();
+            this.cmdGuardar = new System.Windows.Forms.Button();
+            this.cmdSalir = new System.Windows.Forms.Button();
+            this.cboCiudad = new System.Windows.Forms.ComboBox();
+            this.lstClientes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CUIT:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dirección:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Cod. Postal:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 268);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Teléfono:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 202);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Ciudad:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 305);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Mail:";
+            // 
+            // txtCUIT
+            // 
+            this.txtCUIT.Location = new System.Drawing.Point(129, 137);
+            this.txtCUIT.Name = "txtCUIT";
+            this.txtCUIT.Size = new System.Drawing.Size(121, 20);
+            this.txtCUIT.TabIndex = 9;
+            // 
+            // txtCodPostal
+            // 
+            this.txtCodPostal.Location = new System.Drawing.Point(129, 226);
+            this.txtCodPostal.Name = "txtCodPostal";
+            this.txtCodPostal.Size = new System.Drawing.Size(121, 20);
+            this.txtCodPostal.TabIndex = 10;
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(129, 24);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(121, 20);
+            this.txtId.TabIndex = 12;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(129, 67);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 20);
+            this.txtNombre.TabIndex = 13;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(129, 165);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(121, 20);
+            this.txtDireccion.TabIndex = 14;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(129, 105);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(121, 20);
+            this.txtApellido.TabIndex = 15;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(129, 302);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(121, 20);
+            this.txtMail.TabIndex = 16;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(129, 265);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(121, 20);
+            this.txtTelefono.TabIndex = 17;
+            // 
+            // cmdNuevo
+            // 
+            this.cmdNuevo.Location = new System.Drawing.Point(33, 355);
+            this.cmdNuevo.Name = "cmdNuevo";
+            this.cmdNuevo.Size = new System.Drawing.Size(75, 23);
+            this.cmdNuevo.TabIndex = 18;
+            this.cmdNuevo.Text = "Nuevo";
+            this.cmdNuevo.UseVisualStyleBackColor = true;
+            this.cmdNuevo.Click += new System.EventHandler(this.cmdNuevo_Click);
+            // 
+            // cmdModificar
+            // 
+            this.cmdModificar.Location = new System.Drawing.Point(142, 355);
+            this.cmdModificar.Name = "cmdModificar";
+            this.cmdModificar.Size = new System.Drawing.Size(75, 23);
+            this.cmdModificar.TabIndex = 19;
+            this.cmdModificar.Text = "Modificar";
+            this.cmdModificar.UseVisualStyleBackColor = true;
+            // 
+            // cmdEliminar
+            // 
+            this.cmdEliminar.Location = new System.Drawing.Point(254, 355);
+            this.cmdEliminar.Name = "cmdEliminar";
+            this.cmdEliminar.Size = new System.Drawing.Size(75, 23);
+            this.cmdEliminar.TabIndex = 20;
+            this.cmdEliminar.Text = "Eliminar";
+            this.cmdEliminar.UseVisualStyleBackColor = true;
+            // 
+            // cmdGuardar
+            // 
+            this.cmdGuardar.Location = new System.Drawing.Point(365, 355);
+            this.cmdGuardar.Name = "cmdGuardar";
+            this.cmdGuardar.Size = new System.Drawing.Size(75, 23);
+            this.cmdGuardar.TabIndex = 21;
+            this.cmdGuardar.Text = "Guardar";
+            this.cmdGuardar.UseVisualStyleBackColor = true;
+            this.cmdGuardar.Click += new System.EventHandler(this.cmdGuardar_Click);
+            // 
+            // cmdSalir
+            // 
+            this.cmdSalir.Location = new System.Drawing.Point(466, 355);
+            this.cmdSalir.Name = "cmdSalir";
+            this.cmdSalir.Size = new System.Drawing.Size(75, 23);
+            this.cmdSalir.TabIndex = 22;
+            this.cmdSalir.Text = "Salir";
+            this.cmdSalir.UseVisualStyleBackColor = true;
+            this.cmdSalir.Click += new System.EventHandler(this.cmdSalir_Click);
+            // 
+            // cboCiudad
+            // 
+            this.cboCiudad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCiudad.FormattingEnabled = true;
+            this.cboCiudad.Location = new System.Drawing.Point(129, 194);
+            this.cboCiudad.Name = "cboCiudad";
+            this.cboCiudad.Size = new System.Drawing.Size(121, 21);
+            this.cboCiudad.TabIndex = 23;
+            // 
+            // lstClientes
+            // 
+            this.lstClientes.FormattingEnabled = true;
+            this.lstClientes.Location = new System.Drawing.Point(317, 30);
+            this.lstClientes.Name = "lstClientes";
+            this.lstClientes.Size = new System.Drawing.Size(269, 290);
+            this.lstClientes.TabIndex = 24;
+            // 
+            // ABMCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 390);
+            this.Controls.Add(this.lstClientes);
+            this.Controls.Add(this.cboCiudad);
+            this.Controls.Add(this.cmdSalir);
+            this.Controls.Add(this.cmdGuardar);
+            this.Controls.Add(this.cmdEliminar);
+            this.Controls.Add(this.cmdModificar);
+            this.Controls.Add(this.cmdNuevo);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtCodPostal);
+            this.Controls.Add(this.txtCUIT);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ABMCliente";
+            this.Text = "Cliente";
+            this.Load += new System.EventHandler(this.ABMCliente_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtCUIT;
+        private System.Windows.Forms.TextBox txtCodPostal;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Button cmdNuevo;
+        private System.Windows.Forms.Button cmdModificar;
+        private System.Windows.Forms.Button cmdEliminar;
+        private System.Windows.Forms.Button cmdGuardar;
+        private System.Windows.Forms.Button cmdSalir;
+        private System.Windows.Forms.ComboBox cboCiudad;
+        private System.Windows.Forms.ListBox lstClientes;
+    }
+}
---
+++ proyectoPAV/proyectoPAV/ABMCliente.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoPAV
+{
+    public partial class ABMCliente : Form
+    {
+        public ABMCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ABMCliente_Load(object sender, EventArgs e)
+        {
+            //hay que deshabilitar los campos txt 
+            deshabilitarCajasTexto();
+
+            //DEShabilitar los botones que no necesito
+            cmdEliminar.Enabled = false;
+            cmdModificar.Enabled = false;
+            cmdGuardar.Enabled = false;
+            //cargo combos
+            //Ciudad
+            cargoCiudades();
+            //creo una tabla y voy llenando sus filas con los campos que traigo desde la bdd.
+            
+
+            //esto deberia cargar nuestra tabla local
+            tabla = oDatos.consultarTabla("Cliente");
+            lstClientes.DataSource = tabla;
+            lstClientes.ValueMember = tabla.Columns[0].ColumnName;
+            lstClientes.DisplayMember = "n_usuario";
+        }
+        private void deshabilitarCajasTexto()
+            {
+            //todos menos ID
+                txtApellido.Enabled = false;
+                txtNombre.Enabled = false;
+                txtCUIT.Enabled = false;
+                txtDireccion.Enabled = false;
+                txtCodPostal.Enabled = false;
+                txtTelefono.Enabled = false;
+                txtMail.Enabled = false;
+                }
+        private void cargoCiudades()
+            {
+                DataTable tabla = oDatos.consultarTabla("Ciudad");
+                cboCiudad.DataSource = tabla;
+                cboCiudad.valueMember = tabla.Columns[0].ColumnName;
+                cboCiudad.DisplayMember = tabla.Columns[1].ColumnName;
+        }
+        private void habilitarCajasTexto()
+        {
+            //todos menos ID
+            txtApellido.Enabled = true;
+            txtNombre.Enabled = true;
+            txtCUIT.Enabled = true;
+            txtDireccion.Enabled = true;
+            txtCodPostal.Enabled = true;
+            txtTelefono.Enabled = true;
+            txtMail.Enabled = true;
+        }
+
+        private void cmdNuevo_Click(object sender, EventArgs e)
+        {
+            limpiarCliente();
+
+        }
+        private void  limpiarCliente()
+        {
+            txtApellido.Text= string.Empty;
+            txtNombre.Text = string.Empty;
+            txtCUIT.Text = string.Empty;
+            txtDireccion.Text = string.Empty;
+            txtCodPostal.Text = string.Empty;
+            txtTelefono.Text = string.Empty;
+            txtMail.Text = string.Empty;
+            cboCiudad.TabIndex = -1;
+        }
+
+        private void cmdSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void cmdGuardar_Click(object sender, EventArgs e)
+        {
+            //deberia existir un metodo validar()
+            validarCliente();
+            //if (nuevo)
+            //{
+            //insert
+            //la " y ' se usan para cuando se usan Text
+            string sql = "insert into Users (n_usuario, password, email, id_perfil, estado) values ("
+                                            + "'" + txtNombre.Text + "','"
+                                            + "'" + txtContraseña.Text + "','"
+                                            + "'" + txtEmail.Text + "',"
+                                            + cmbPerfil.selectedValue + ", 'S')";
+
+
+            //}
+            //else
+            //{   
+            //update
+            //}
+            //nuevo=false;
+            //actualizarLista()
+            //poner un cartel q avise q ya se grabo o modifico
+            //como ya grabe tengo q volver al mismo estado incial:
+            //hay que deshabilitar los campos txt 
+            //habilitar los botones que no necesito
+
+            //hay q verificar si el q estoy grabando es un usuario nuevo o alguno q estoy modificando, lo puedo hacer a traves de una bandera
+        }
+        private bool validarCliente()
+            //hay que ver cuales pueden ser null y cual no
+        {
+            if (txtApellido.Text != string.Empty)
+            {
+                Message("Ingrese Apeliido");
+                return false;
+            }
+            if (txtNombre.Text != string.Empty)
+            {
+                Message("Ingrese Nombre");
+                return false;
+            }
+            return true;
+        }      
+    }
+}
